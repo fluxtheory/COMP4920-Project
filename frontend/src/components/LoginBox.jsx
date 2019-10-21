7ae8b4ac7f4117ff28d2bc74d03f8b58a7fc2a52
Jordan-I <jordani.general@gmail.com>
2019-10-21 20:55:44 +1100

    Login redirect to dashboard

--- frontend/src/components/LoginBox.jsx
+++ frontend/src/components/LoginBox.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { TextField, Button } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   creationContainer: {
@@ -29,9 +30,53 @@ function LoginBox() {
     password: '',
   });
 
+  const [loginHelpString, setLoginHelpString] = React.useState('');
+  const [loginSuccess, setLoginSuccess] = React.useState(false);
+
   const handleChange = name => event => {
     setValues({ ...values, [name]: event.target.value });
   };
+
+  const handleSubmit = e => {
+    sendPostValues();
+    e.preventDefault();
+  };
+
+  const sendPostValues = () => {
+    var xhr = new XMLHttpRequest();
+    var url = 'http://localhost:3001/api/users/login';
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json);
+        if (json.success === true) {
+          setLoginSuccess(true);
+        } else {
+          setLoginHelpString('Invalid email/password.');
+        }
+      }
+    };
+    var json = {};
+    if (values.nameOrEmail.includes('@')) {
+      json = {
+        email: values.nameOrEmail,
+        password: values.password,
+      };
+    } else {
+      json = {
+        name: values.nameOrEmail,
+        password: values.password,
+      };
+    }
+    var data = JSON.stringify(json);
+    xhr.send(data);
+  };
+
+  if (loginSuccess === true) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <div className={classes.creationContainer}>
@@ -39,7 +84,7 @@ function LoginBox() {
         <Typography variant="h5" component="h3">
           Enter your details:
         </Typography>
-        <form>
+        <form onSubmit={e => handleSubmit(e)}>
           <div className={classes.textFieldContainer}>
             <TextField
               id="nameOrEmail"
@@ -53,6 +98,7 @@ function LoginBox() {
               autoComplete="off"
               required
             />
+            <Typography variant="subtitle1">{loginHelpString}</Typography>
           </div>
           <div className={classes.textFieldContainer}>
             <TextField
